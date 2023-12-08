e182d56904cd09659750a075067c5ba674d06004
IskalAlrozhan <iskal.alrozhan@gmail.com>
2023-12-08 14:55:00 +0800

    add welcome screen

--- app/src/main/java/com/wowrack/cloudrayaapps/ui/components/NotificationList.kt
+++ app/src/main/java/com/wowrack/cloudrayaapps/ui/components/NotificationList.kt
@@ -1,8 +1,10 @@
 package com.wowrack.cloudrayaapps.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,9 +16,9 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -141,14 +143,20 @@ fun NotificationListHome(
     ) {
         if (data.isEmpty()) {
             item {
-                Text(
-                    text = "No Notification",
-                    fontFamily = poppinsBold,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(16.dp),
-                    color = MaterialTheme.colorScheme.primary
-                )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "No Notification",
+                        fontFamily = poppinsBold,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(16.dp),
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
             }
         } else {
             items(data, key = { it.id }) { notification ->
--- app/src/main/java/com/wowrack/cloudrayaapps/ui/screen/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/wowrack/cloudrayaapps/ui/screen/welcome/WelcomeScreen.kt
@@ -1,14 +1,28 @@
 package com.wowrack.cloudrayaapps.ui.screen.welcome
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.wowrack.cloudrayaapps.R
 import com.wowrack.cloudrayaapps.ui.common.UiState
 import com.wowrack.cloudrayaapps.ui.common.getViewModelFactory
+import com.wowrack.cloudrayaapps.ui.theme.poppinsBold
 
 @Composable
 fun WelcomeScreen(
@@ -43,5 +57,16 @@ fun WelcomeScreen(
         }
     }
 
-    Text(text = "test")
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.cloudraya_login_logo),
+            contentDescription = "image",
+            Modifier.size(150.dp)
+        )
+    }
 }
